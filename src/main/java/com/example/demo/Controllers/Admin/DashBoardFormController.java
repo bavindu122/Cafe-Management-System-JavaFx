6359cd1257c7f9ece7eaccc97a3cc2f07d157385
Bavindu Shamen <140163711+bavindu122@users.noreply.github.com>
2024-11-08 11:08:38 +0530

    before fixin the fked up

---
+++ src/main/java/com/example/demo/Controllers/Admin/DashBoardFormController.java
@@ -0,0 +1,40 @@
+package com.example.demo.Controllers.Admin;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class DashBoardFormController {
+    public MFXButton btnDashboard;
+    public MFXButton btnOrder;
+    public MFXButton btnMenuItems;
+    public MFXButton btnCustomer;
+    public MFXButton btnSales;
+    public MFXButton btnInventory;
+    public MFXButton btnLogout;
+    public AnchorPane mainPane;
+
+    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMenuItemsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSalesOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnInventoryOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+}
