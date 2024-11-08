56e00565468cfd82f4ad383297baac371311b414
Bavindu Shamen <140163711+bavindu122@users.noreply.github.com>
2024-11-08 11:14:39 +0530

    after adding missing DashboardController

--- src/main/java/com/example/demo/Controllers/Admin/DashBoardFormController.java
+++ src/main/java/com/example/demo/Controllers/Admin/DashBoardFormController.java
@@ -2,7 +2,12 @@ package com.example.demo.Controllers.Admin;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class DashBoardFormController {
     public MFXButton btnDashboard;
@@ -13,14 +18,39 @@ public class DashBoardFormController {
     public MFXButton btnInventory;
     public MFXButton btnLogout;
     public AnchorPane mainPane;
+    public MFXButton btnEmployee;
 
-    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    public void initialize() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Admin/dashBoardMainForm.fxml"));
+
+        Pane registerPane = (Pane) fxmlLoader.load();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().add(registerPane);
+
+        //To Activate dashboard Button
+        setButtonActive(btnDashboard);
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        setButtonActive(btnDashboard);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Admin/dashBoardMainForm.fxml"));
+        Pane registerPane = (Pane) fxmlLoader.load();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().add(registerPane);
+
     }
 
     public void btnOrderOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    @FXML
+    void btnEmployeeOnAction(ActionEvent event) throws IOException {
+        setButtonActive(btnEmployee);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Admin/employeeForm.fxml"));
+        Pane registerPane = (Pane) fxmlLoader.load();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().add(registerPane);
     }
 
     public void btnMenuItemsOnAction(ActionEvent actionEvent) {
@@ -36,5 +66,22 @@ public class DashBoardFormController {
     }
 
     public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+    private void setButtonActive(MFXButton activeButton) {
+
+        //Remove Style
+        btnDashboard.getStyleClass().removeAll("mfx-active-button","mfx-button");
+
+
+        btnOrder.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnSales.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnEmployee.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnCustomer.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnLogout.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnMenuItems.getStyleClass().removeAll("mfx-active-button","mfx-button");
+        btnInventory.getStyleClass().removeAll("mfx-active-button","mfx-button");
+
+        //Add Style
+        activeButton.getStyleClass().add("mfx-active-button");
     }
 }
